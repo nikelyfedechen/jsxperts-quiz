ffaa6fc57ac6bd2affa696c98e22d7887b19d955
Nikely Fedechen <fedechenikely@gmail.com>
2025-06-19 19:58:03 -0300

    feat: adiciona Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header__container">
+      <h1>JSXperts</h1>
+      <h3>O Quiz dos Devs React</h3>
+    </div>
+  );
+};
+
+export default Header;
